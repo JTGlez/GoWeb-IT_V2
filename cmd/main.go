5c007b142439feb07bab969ffc1a591895d55dc9
JTGlez <jorjeluigi1@gmail.com>
2024-10-17 10:00:35 -0600

    feat: add godotenv dependency

--- cmd/main.go
+++ cmd/main.go
@@ -4,28 +4,40 @@ import (
 	"github.com/JTGlez/GoWeb-IT_V2/cmd/server/handler/ping"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Router init
 	rt := chi.NewRouter()
 	rt.Use(middleware.Logger)
 	rt.Use(middleware.Recoverer)
 
+	// Service handlers
 	svcPong := ping.NewHandler()
 
+	// Routes
 	rt.Route("/ping", func(r chi.Router) {
 		r.Get("/", svcPong.GetPong)
 	})
 
+	// Server configs
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = ":8080"
 		log.Printf("HOST env not set, using default: %s", port)
 	}
 
+	// Startup
 	log.Printf("Starting application on %s", port)
 	if err := http.ListenAndServe(port, rt); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/JTGlez/GoWeb-IT_V2
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
